6ea62577cc5178040577bf64e3679691471925f5
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-08-17 01:26:53 +0300

    Add files via upload

---
+++ src/test/java/com/otekin/dev/demo/springactuatorcustomhealthindicator/SpringActuatorCustomHealthIndicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otekin.dev.demo.springactuatorcustomhealthindicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorCustomHealthIndicatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
